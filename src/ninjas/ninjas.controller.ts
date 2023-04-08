6377eff682a54dcc327e2acad0a1a72a18c189a9
Marcel "Malib" Beliš <belis.marcel@gmail.com>
2023-04-08 11:14:30 +0200

    Added CRUD for ninjas.controller.ts

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,35 @@
+import {Controller, Get, Post, Put, Delete} from '@nestjs/common';
+
+@Controller('ninjas')
+export class NinjasController {
+
+    // GET /ninjas/ --> []
+  @Get()
+  getNinjas() {
+    return [];
+  }
+
+    // GET /ninjas/:id --> {...}
+  @Get(':id')
+    getOneNinja(){
+    return {};
+  }
+
+  // POST /ninjas
+    @Post()
+    createNinja(){
+      return {};
+    }
+
+    // PUT ninjas/:id --> {...}
+    @Put(':id')
+    updateNinja(){
+      return {};
+    }
+
+    // DELETE /ninjas/:id
+    @Delete(':id')
+    removeNinja(){
+      return {};
+    }
+}
