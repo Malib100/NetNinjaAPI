766b584bcce9786264fa7041ca7d0c8e0bfcb715
Marcel "Malib" Beliš <belis.marcel@gmail.com>
2023-04-08 15:01:17 +0200

    Pipes

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,5 +1,9 @@
+import {IsEnum, MinLength} from 'class-validator';
+
 export class CreateNinjaDto {
+    @MinLength(3)
     name: string;
 
+    @IsEnum(['stars', 'nunchuks'], {message:'Use the right weapon buddy!'})
     weapon: 'stars' | 'nunchuks'
 }
--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Post, Put, Delete, Param, Query, Body, NotFoundException, ImATeapotException} from '@nestjs/common';
+import {Controller, Get, Post, Put, Delete, Param, Query, Body, NotFoundException, ImATeapotException, ParseIntPipe, ValidationPipe} from '@nestjs/common';
 import {CreateNinjaDto} from "./dto/create-ninja.dto";
 import {UpdateNinjaDto} from "./dto/update-ninja.dto";
 import {NinjasService} from "./ninjas.service";
@@ -19,11 +19,11 @@ export class NinjasController {
 
     // GET /ninjas/:id --> {...}
   @Get(':id')
-    getOneNinja(@Param('id') id:string){
+    getOneNinja(@Param('id', ParseIntPipe) id:number){
 
     try {
 
-      return this.ninjasService.getNinja(+id);
+      return this.ninjasService.getNinja(id);
 
     } catch (err) {
       throw new ImATeapotException()
@@ -32,7 +32,7 @@ export class NinjasController {
 
   // POST /ninjas
     @Post()
-    createNinja(@Body() createNinjaDto: CreateNinjaDto){
+    createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto){
 
       return this.ninjasService.createNinja(createNinjaDto);
     }
